2e3120bbb4df86e4e92b19209bd17047bcd6a6fd
Mikhail Babaychev <109237708+MikiHousse@users.noreply.github.com>
2022-11-05 15:53:25 +0300

    Завершение работы

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,13 @@ const sass        = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
-const imagemin = require('gulp-imagemin');
+// const imagemin = require('gulp-imagemin');
 const htmlmin = require('gulp-htmlmin');
+
+// import gulp from 'gulp';
+// import browserSync from 'browser-sync';
+// import sass from 'gulp-sass';
+// import imagemin from 'gulp-imagemin';
 
 gulp.task('server', function() {
 
@@ -18,6 +23,8 @@ gulp.task('server', function() {
     gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
+// exports.server = server;
+
 gulp.task('styles', function() {
     return gulp.src("src/sass/**/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
@@ -28,14 +35,18 @@ gulp.task('styles', function() {
         .pipe(browserSync.stream());
 });
 
+// exports.styles = styles;
+
 gulp.task('watch', function() {
     gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
     gulp.watch("src/*.html").on('change', gulp.parallel('html'));
     gulp.watch("src/js/**/*.js").on('change', gulp.parallel('scripts'));
     gulp.watch("src/fonts/**/*").on('all', gulp.parallel('fonts'));
-    gulp.watch("src/icons/**/*").on('all', gulp.parallel('icons'));
-    gulp.watch("src/img/**/*").on('all', gulp.parallel('images'));
+    // gulp.watch("src/icons/**/*").on('all', gulp.parallel('icons'));
+    // gulp.watch("src/img/**/*").on('all', gulp.parallel('images'));
 });
+
+// exports.watch = watch;
 
 gulp.task('html', function () {
     return gulp.src("src/*.html")
@@ -63,9 +74,9 @@ gulp.task('icons', function () {
 
 gulp.task('images', function () {
     return gulp.src("src/img/**/*")
-        .pipe(imagemin())
+        // .pipe(imagemin())
         .pipe(gulp.dest("dist/img"))
         .pipe(browserSync.stream());
 });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'icons', 'html', 'images'));
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'html','icons','images'));
